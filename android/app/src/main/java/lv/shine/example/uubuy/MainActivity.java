f24f58113c8da0a2ea3c4b47ec378698c369ef70
LvYang <tylvyang@qq.com>
2019-01-02 20:07:11 +0800

    init flutter

---
+++ android/app/src/main/java/lv/shine/example/uubuy/MainActivity.java
@@ -0,0 +1,13 @@
+package lv.shine.example.uubuy;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
